51776448dadcfb8b6aae74c3cd0abb1e03419953
louiechristie <post@louiechristie.com>
2014-04-28 16:22:58 +0100

    games_app game-move association

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ActiveRecord::Base
   attr_accessible :challenger_id, :invitee_id, :next_turn_id, :score, :winner_id
+
+  has_many :moves
 end
--- app/models/move.rb
+++ app/models/move.rb
@@ -1,3 +1,5 @@
 class Move < ActiveRecord::Base
   attr_accessible :game_id, :square, :user_id
+
+  belongs_to :game
 end
