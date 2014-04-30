75414f8042dd9986a6a223e9fff678c440116e08
louiechristie <post@louiechristie.com>
2014-04-30 14:40:55 +0100

    games_app Click on board, some validations

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -3,9 +3,15 @@ class MovesController < ApplicationController
 
   def create
     
-    @move = Move.create(params[:move]) 
-    @game = Game.find(params[:move][:game_id])
+    @game = Game.find(params[:move].delete(:game_id))
+
+    @move = @game.moves.build(params[:move]) 
+
+    unless @move.save
+      flash[:notice] = "Could not make move due to the following errors: " + @move.errors.full_messages.to_sentence
+    end
     redirect_to @game
+
   end
 
 end
--- app/models/move.rb
+++ app/models/move.rb
@@ -8,4 +8,23 @@ class Move < ActiveRecord::Base
   validates :user_id, presence: true
   validates :square, presence: true
 
+  # is game finished
+  validate :game_in_progress
+  validate :square_is_empty
+
+  # is user_id a player of the game
+
+  # is user_id is it their turn
+
+  def square_is_empty
+  errors.add(:square, "is already played.") if game&& game.board[square]
+  end
+
+
+  # is square within bounds
+
+  def game_in_progress
+    errors.add(:game, "is finished") if game&& game.game_is_finished?
+  end
+
 end
